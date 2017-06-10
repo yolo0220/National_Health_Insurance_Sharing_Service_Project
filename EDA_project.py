bf6ca9ffb015a597c99ac55fa0f5abe1f404435a
Jihoon Kim <jihoon_kim@outlook.com>
2017-06-11 01:27:49 +0900

    ANALYSIS on t20.MAIN_SICK

--- EDA_project.py
+++ EDA_project.py
@@ -10,6 +10,7 @@ Created on Sat Jun 10 20:20:00 2017
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 import dask.dataframe as dd
 from dask.diagnostics import ProgressBar
 pbar = ProgressBar()
@@ -25,3 +26,11 @@ t60 = t60_part1.append(t60_part2)  # Prescription Data
 
 # Example
 sns.countplot(gj.HEIGHT.compute())  # Height Distribution
+
+# Count TOP 10 Main Sick
+main_sick_count = t20.MAIN_SICK.value_counts().compute()
+top10_main_sick = main_sick_count[:10]
+
+# t20 table confined to top 10 main sick:
+t20_top10_sick = t20[t20.MAIN_SICK.isin(top10_main_sick.index)].compute()
+sns.countplot(x="MAIN_SICK", hue="SEX", data=t20_top10_sick)
