9bb2a2f24fc42412eb404a15425a9f96647f7156
Jihoon Kim <jihoon_kim@outlook.com>
2017-06-10 20:41:03 +0900

    base data comment added

--- EDA_project.py
+++ EDA_project.py
@@ -16,12 +16,12 @@ pbar = ProgressBar()
 pbar.register()
 
 # Load Data
-gj = dd.read_csv('./data/NHIS_OPEN_GJ_2015.CSV')
-t20 = dd.read_csv('./data/NHIS_OPEN_T20_2015.CSV')
+gj = dd.read_csv('./data/NHIS_OPEN_GJ_2015.CSV')  # Medical Examination Data
+t20 = dd.read_csv('./data/NHIS_OPEN_T20_2015.CSV')  # Treatment Data
 t60_part1 = dd.read_csv('./data/NHIS_OPEN_T60_2015_part1.CSV')
 t60_part2 = dd.read_csv('./data/NHIS_OPEN_T60_2015_part2.CSV')
-t60 = t60_part1.append(t60_part2)
+t60 = t60_part1.append(t60_part2)  # Prescription Data
 
 
 # Example
-sns.countplot(gj.HEIGHT.compute()) # Height Distribution
+sns.countplot(gj.HEIGHT.compute())  # Height Distribution
