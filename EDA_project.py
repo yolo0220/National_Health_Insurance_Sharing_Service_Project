44f7d615b17e2bd1873b2dc9a7c495e5f45f67bb
Jihoon Kim <jihoon_kim@outlook.com>
2017-06-12 18:56:33 +0900

    t20, gj: feature 추가

--- EDA_project.py
+++ EDA_project.py
@@ -27,6 +27,12 @@ t60_part1 = dd.read_csv('./data/NHIS_OPEN_T60_2015_part1.CSV')
 t60_part2 = dd.read_csv('./data/NHIS_OPEN_T60_2015_part2.CSV')
 t60 = t60_part1.append(t60_part2)  # Prescription Data
 
+# Preprocessing
+t20["MAIN_SICK_CODE"] = t20.MAIN_SICK[t20.MAIN_SICK.notnull()].apply(
+        lambda x: x[0])
+t20["SUB_SICK_CODE"] = t20.SUB_SICK[t20.SUB_SICK.notnull()].apply(
+        lambda x: x[0])
+t20.compute()
 
 # Count TOP 10 Main Sick
 main_sick_count = t20.MAIN_SICK.value_counts().compute()
@@ -72,11 +78,6 @@ plt.ylabel("주상병코드")
 plt.show()
 
 # MAIN_SICK, SUB_SICK / HEATMAP BY CODE
-t20["MAIN_SICK_CODE"] = t20.MAIN_SICK[t20.MAIN_SICK.notnull()].apply(
-        lambda x: x[0])
-t20["SUB_SICK_CODE"] = t20.SUB_SICK[t20.SUB_SICK.notnull()].apply(
-        lambda x: x[0])
-
 # TOP 10 MAIN_SICK, SUB_SICK BY CODE
 main_sick_code_count = t20.MAIN_SICK_CODE.value_counts().compute()
 top10_main_sick_code = main_sick_code_count[:10]
@@ -85,13 +86,14 @@ t20_top10_sick_code = t20[t20.MAIN_SICK_CODE.isin(
 
 t20_top10_sick_with_sub_sick_code = t20_top10_sick_code.SUB_SICK_CODE.value_counts().compute()
 t20_top10_sick_with_sub_sick_code = t20_top10_sick_with_sub_sick_code[:20]
-t20_main_sub_sick_code = t20_top10_sick_code[t20_top10_sick_code.SUB_SICK_CODE.isin(
-        t20_top10_sick_with_sub_sick_code.index)].compute()
+t20_main_sub_sick_code = t20_top10_sick_code[t20_top10_sick_code.SUB_SICK_CODE.isin(t20_top10_sick_with_sub_sick_code.index)].compute()
 
 plt.figure(figsize=(18, 12))
-main_sub_sick_size_code = t20_main_sub_sick_code.pivot_table(index="MAIN_SICK_CODE",
-                                                             columns="SUB_SICK_CODE",
-                                                             aggfunc="size")
+main_sub_sick_size_code = t20_main_sub_sick_code.pivot_table(
+        index="MAIN_SICK_CODE",
+        columns="SUB_SICK_CODE",
+        aggfunc="size"
+        )
 sns.heatmap(main_sub_sick_size_code, annot=True, fmt="d")
 plt.xticks(rotation=45)
 plt.title("주상병-부상병 관계")
